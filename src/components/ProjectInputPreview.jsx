8cb84302d9f980a1ad00a2908482b7010140ea72
Leandro Rufino <leandrix1234@gmail.com>
2024-11-20 22:32:35 -0300

    img src leveraging build process

--- src/components/ProjectInputPreview.jsx
+++ src/components/ProjectInputPreview.jsx
@@ -1,10 +1,15 @@
 import Button from "./Button";
 import Section from "./Section";
+import noProjectSelectedImg from "../assets/no-projects.png";
 
 export default function ProjectInputPreview({ onChangeContentDisplayed }) {
   return (
     <Section>
-      <img className=" max-w-20 max-h-20" src="/logo.png" alt="note-read" />
+      <img
+        className=" max-w-20 max-h-20"
+        src={noProjectSelectedImg}
+        alt="note-read"
+      />
       <h1 className=" font-bold uppercase">No project selected</h1>
       <p>Please select a project or get started with a new one</p>
       <Button
